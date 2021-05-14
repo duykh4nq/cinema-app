3c76701e4b940b4d572db7b30102ae635bb96603
Trúc Mai <69801648+NTTMAI20@users.noreply.github.com>
2021-05-14 22:25:55 +0700

    NTTMAI20 update router.js

--- package/frontend/src/routers/router.js
+++ package/frontend/src/routers/router.js
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router } from "react-router-dom";
-
+import Header from "../layouts/Header/Header.js";
+import Footer from "../layouts/Footer/Footer.js";
 //component
 import HomeRoute from "./home.router";
 import LoginScreen from "../pages/Loginpage/login.page";
+
 function Routers() {
   return (
-    <Router>
+    <Router >
+      <Header />
       <LoginScreen />
       <HomeRoute />
+      <Footer />
     </Router>
   );
 }
