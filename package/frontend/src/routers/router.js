d56e97d6cc7ce0f58d8fccd0cf17690c6223c8b0
Trúc Mai <69801648+NTTMAI20@users.noreply.github.com>
2021-05-18 15:33:33 +0700

    Update router.js

--- package/frontend/src/routers/router.js
+++ package/frontend/src/routers/router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../layouts/header/header.component";
+import Footer from "../layouts/footer/footer.component";
+import Detail from "../pages/Detailpage/detail.page";
 
 function Routes(props) {
     return (
         <Router>
-            <Header></Header>
-    </Router>
+            <Header />
+            <Detail />
+            <Footer />
+        </Router>
     );
 }
 
