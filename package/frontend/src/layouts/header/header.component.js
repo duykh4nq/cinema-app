60b513026205f4a3aa9893efc0a6d8035f4ed0c5
nhnghia1119 <nhnghia1119@gmail.com>
2021-05-25 22:36:15 +0700

    [NEW] add search header & new homepage

--- package/frontend/src/layouts/header/header.component.js
+++ package/frontend/src/layouts/header/header.component.js
@@ -197,6 +197,17 @@ function HeaderComponent() {
             </ul>
           </div>
         </nav>
+
+        <div class="top-search">
+          <select>
+            <option value="united">TV show</option>
+            <option value="saab">Others</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Search for a movie, TV Show or celebrity that you are looking for"
+          />
+        </div>
       </div>
     </header>
   );
