1e4fa31500f5fad9a2ea3b65f3a09b2157ecc9c8
unknown <maitruc657@gmail.com>
2021-06-11 11:15:11 +0700

    data movies

--- package/backend/configs/db.js
+++ package/backend/configs/db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 module.exports = new Sequelize(
     process.env.DATABASE_URL ||
-    "postgres://postgres:123@localhost:5432/cinema", {
+    "postgres://postgres:123@localhost:5432/cinema-app", {
         dialect: "postgres",
         protocol: "postgres",
         // dialectOptions: {
