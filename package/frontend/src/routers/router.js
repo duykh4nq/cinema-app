2ea1b57685788f40782f6d5f7134e7302ce40920
Trúc Mai <69801648+NTTMAI20@users.noreply.github.com>
2021-06-11 11:16:48 +0700

    Update router.js

--- package/frontend/src/routers/router.js
+++ package/frontend/src/routers/router.js
@@ -7,18 +7,16 @@ import BookingRoute from "./booking.router";
 import HomeRoute from "./home.router";
 import CheckoutRoute from "./checkout.router";
 import AboutUsRoute from "./aboutus.router";
-import Homepage from "../pages/Homepage/home.page";
 
 function Routes() {
   return (
     <Router>
       <Header />
-      <Homepage/>
-      {/* <HomeRoute /> */}
-      {/* <DetailRoute />
+       <HomeRoute /> 
+      <DetailRoute />
       <AboutUsRoute />
       <BookingRoute />
-      <CheckoutRoute /> */}
+      <CheckoutRoute /> 
       <Footer />
     </Router>
   );
