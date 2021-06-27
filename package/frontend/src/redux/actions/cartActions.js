3955859e9b87f87f510e546019b17688c27b63cf
Khang Duy <nguyenkhang.051200@gmail.com>
2021-06-27 20:14:26 +0700

    update jkadkda

--- package/frontend/src/redux/actions/cartActions.js
+++ package/frontend/src/redux/actions/cartActions.js
@@ -1,11 +1,11 @@
 import * as actionTypes from "../constants/cartConstants";
 import axios from "../configAxios";
 
-export const checkoutCart = (total, seat) => async (dispatch, getState) => {
+export const checkoutCart = (total, seat) => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.CHECK_OUT_REQUEST });
     const id_seat = JSON.parse(sessionStorage.getItem("movies"));
-    const { data } = await axios.post(`booking/seat`, {
+    const { data } = await axios.post(`/payment`, {
       id_schedule: id_seat.id,
       total: total,
       seat: seat,
